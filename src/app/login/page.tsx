489b980cb86388d129b119d1f6ee21ba4c00591c
KARTIK SAXENA <92146206+kartiksaxena532@users.noreply.github.com>
2023-08-20 23:42:05 +0530

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage(){
+
+
+    return(
+        <div>
+            <h1 className="text-[50px] text-center">Login Page</h1>
+        </div>
+    )
+}
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,9 @@
+export default function SignupPage(){
+
+
+    return(
+        <div>
+            <h1 className="text-[50px] text-center">Sign Up Page</h1>
+        </div>
+    )
+}
